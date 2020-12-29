99d9cf733e1455f12926a05ef9b5f99b3e4d88d4
rus_anonym <alexandrsemin2033@gmail.com>
2020-12-30 01:53:55 +0300

    create class Task

--- src/lib/api/tasks.ts
+++ src/lib/api/tasks.ts
@@ -1,34 +1,63 @@
 /**
- * @param {Object} params - Набор параметров для создания задачи
- * @param {Date | number} params.plannedTime - Время когда должна выполниться задача
- * @param {string} params.type - Тип задачи, ни на что не влияет, но можно использовать для получения списка задач с выбранным типом
- * @param {Object} params.params - Дополнительные параметры, для поиска задач
- * @param {boolean} params.inform - Информирование о завершении/ошибке после выполнения задачи
- * @param {boolean} params.isInterval - Является ли данная задача интервалом
- * @param {number} params.intervalTimer - Промежуток выполнения интервала в ms
- * @param {number} params.intervalTriggers - Количество срабатываний интервала, после чего он должен завершиться
- * @param {boolean} params.boolean - Сохранять ли эту задачу в автоматическом режиме
+ * @module Tasks
+ * @description Методы для взаимодействия с задачами
  */
-function create({
-	plannedTime,
-	type = "missing",
-	params: {},
-	inform = false,
-	isInterval = false,
-	intervalTimer = Number(plannedTime) - Number(new Date()),
-	intervalTriggers = 0,
-	backup = false,
-}: {
-	plannedTime: Date | number;
-	type: string;
-	params: Record<string, any>;
-	inform: boolean;
-	isInterval: boolean;
-	intervalTimer?: number;
-	intervalTriggers?: number;
-	backup: boolean;
-}) {
-    
+
+import { create } from "../tasks";
+
+/**
+ * @class
+ * @classdesc Это конструктор для задач
+ */
+class Task {
+	/**
+	 * @param {Object} params - Набор параметров для создания задачи
+	 * @param {Date | number} params.plannedTime - Время когда должна выполниться задача
+	 * @param {string} params.type - Тип задачи, ни на что не влияет, но можно использовать для получения списка задач с выбранным типом
+	 * @param {Object} params.params - Дополнительные параметры, для поиска задач
+	 * @param {boolean} params.inform - Информирование о завершении/ошибке после выполнения задачи
+	 * @param {boolean} params.isInterval - Является ли данная задача интервалом
+	 * @param {number} params.intervalTimer - Промежуток выполнения интервала в ms
+	 * @param {number} params.intervalTriggers - Количество срабатываний интервала, после чего он должен завершиться
+	 * @param {boolean} params.boolean - Сохранять ли эту задачу в автоматическом режиме
+	 */
+	constructor({
+		plannedTime,
+		type = "missing",
+		params = {},
+		inform = false,
+		isInterval = false,
+		intervalTimer = Number(plannedTime) - Number(new Date()),
+		intervalTriggers = 0,
+		backup = false,
+		source,
+	}: {
+		plannedTime: Date | number;
+		type: string;
+		params: Record<string, any>;
+		inform: boolean;
+		isInterval: boolean;
+		intervalTimer?: number;
+		intervalTriggers?: number;
+		backup: boolean;
+		source: () => void;
+	}) {
+		if (!plannedTime || !source) {
+			throw new Error("One of the required parameters is not specified");
+		}
+		return create({
+			plannedTime: Number(plannedTime),
+			type: type,
+			params: params,
+			inform: inform,
+			isInterval: isInterval,
+			intervalTimer: intervalTimer,
+			intervalTriggers: intervalTriggers,
+			backup: backup,
+			service: false,
+			source: source,
+		});
+	}
 }
 
-export { create };
+export { Task };
