e00a17b66d7adaf44deaac02b7aee9c44585e87b
RusAnonym <alexandrsemin2033@gmail.com>
2021-03-02 20:17:18 +0300

    add TaskConstructor export

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import * as core from "./lib/core";
 const settings = core.settings;
 const tasks = core.TasksAPI;
 const events = core.Events;
+const TaskConstructor = core.TasksAPI.Task;
 
-export { settings, tasks, events };
+export { settings, tasks, events, TaskConstructor as Task };
