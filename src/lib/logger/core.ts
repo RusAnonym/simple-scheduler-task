9507b3bc372d16d29e4ba23901955a193d20ad61
RusAnonym <alexandrsemin2033@gmail.com>
2021-07-15 02:16:26 +0300

    update logger class

--- src/lib/logger/core.ts
+++ src/lib/logger/core.ts
@@ -10,6 +10,10 @@ interface SchedulerLogger {
 	on(event: "log", listener: (data: TSchedulerLog) => void): this;
 	on(event: "done", listener: (data: ISchedulerLogInform) => void): this;
 	on(event: "error", listener: (data: ISchedulerLogError) => void): this;
+
+	emit(event: "log", data: TSchedulerLog): boolean;
+	emit(event: "done", data: ISchedulerLogInform): boolean;
+	emit(event: "error", data: ISchedulerLogError): boolean;
 }
 
 class SchedulerLogger extends EventEmitter {}
