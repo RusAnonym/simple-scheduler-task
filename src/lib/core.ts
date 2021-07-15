41125b19b3e36b0e297c01f5be01dfdfa7fddb76
RusAnonym <alexandrsemin2033@gmail.com>
2021-07-15 14:22:04 +0300

    add settings class

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,5 +1,6 @@
 import SchedulerTasks from "./tasks/core";
 import SchedulerLogger from "./logger/core";
+import SchedulerSettings from "./settings/core";
 
 import { ISchedulerConfig } from "./../types/scheduler";
 
@@ -12,6 +13,7 @@ class Scheduler {
 
 	public tasks = new SchedulerTasks();
 	public logger = new SchedulerLogger();
+	public settings = new SchedulerSettings();
 }
 
 const scheduler = new Scheduler();
--- src/lib/settings/core.ts
+++ src/lib/settings/core.ts
@@ -1,14 +1,29 @@
 import scheduler from "../core";
 
 class SchedulerSettings {
-	// public useInterval(): void {
-	// 	if (scheduler.config.mode === "interval") {
-	// 		return;
-	// 	} else {
-	// 		scheduler.config.mode = "interval";
-	// 		scheduler.config.interval = setInterval(() => {},
-	// 		scheduler.config.intervalMS);
-	// 		return;
-	// 	}
-	// }
+	public useInterval(): void {
+		if (scheduler.config.mode === "interval") {
+			return;
+		} else {
+			scheduler.config.mode = "interval";
+			scheduler.tasks.clearTimeouts();
+			scheduler.config.interval = setInterval(() => {
+				scheduler.tasks.executeOutdatedTasks();
+			}, scheduler.config.intervalMS);
+			return;
+		}
+	}
+	public useTimeouts(): void {
+		if (scheduler.config.mode === "timeout") {
+			return;
+		} else {
+			scheduler.config.mode = "timeout";
+			if (scheduler.config.interval !== null) {
+				clearInterval(scheduler.config.interval);
+			}
+			scheduler.tasks.setTimeouts();
+		}
+	}
 }
+
+export default SchedulerSettings;
